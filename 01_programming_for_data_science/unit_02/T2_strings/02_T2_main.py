e5b481869ae4270e382d3374c86270b5bdeefdeb
Mateus Med <mateus.medeiros46@gmail.com>
2020-09-15 15:45:14 -0300

    module 02_T1, 02_T2

---
+++ 01_programming_for_data_science/unit_02/T2_strings/02_T2_main.py
@@ -0,0 +1,26 @@
+
+
+
+def strings_example():
+    # com aspas simples
+    hello_world = 'Hello world!!!'
+
+    # com aspas duplas
+    hello_world = "Hello world!!!"
+
+    im_teacher = "I'm a teacher."
+    variable = 'Ele é "bonito"... kkk'
+
+    nome = "Joaquim"
+    print(name)
+
+    type(name)
+    len(name)
+
+    s = "Python"
+    print(s[0])  # P
+    print(s[1])  # y
+    print(s[2])  # t
+    print(s[3])  # h
+    print(s[4])  # o
+    print(s[5])  # n
